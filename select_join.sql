ef152778f64c8d6386ad53c4e71beca48b0d8467
dominika-kotecka <123742214+dominika-kotecka@users.noreply.github.com>
2023-09-21 19:00:17 +0200

    Update select_join.sql

--- select_join.sql
+++ select_join.sql
@@ -1,9 +1,7 @@
-Select * from employees;
-
 -- Wyświetl imię oraz nazwisko wszystkich pracowników, nazwę działu, w którym pracują a także nazwę miasta, w którym siedzibę ma dany dział.
 
-Select concat(e.first_name, " ",e.last_name) as employee,
+SELECT  concat(e.first_name, " ",e.last_name) as employee,
  d.department_name, l.city
-from employees as e 
-inner join departments as d on e.department_id=d.id
-inner JOIN locations as l on d.location_id = l.id;
+FROM employees AS e 
+INNER JOIN departments AS d ON e.department_id=d.id
+INNER JOIN locations ON l ON d.location_id = l.id;
