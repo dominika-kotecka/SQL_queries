5caf0ce2e2d1ad1ae3bb5231ae18c43b4c1a99c4
dominika-kotecka <123742214+dominika-kotecka@users.noreply.github.com>
2023-09-21 19:49:42 +0200

    Add files via upload

--- select_join.sql
+++ select_join.sql
@@ -1,7 +1,25 @@
+
 -- Wyświetl imię oraz nazwisko wszystkich pracowników, nazwę działu, w którym pracują a także nazwę miasta, w którym siedzibę ma dany dział.
 
-SELECT  concat(e.first_name, " ",e.last_name) as employee,
+Select concat(e.first_name, " ",e.last_name) as employee,
  d.department_name, l.city
-FROM employees AS e 
-INNER JOIN departments AS d ON e.department_id=d.id
-INNER JOIN locations ON l ON d.location_id = l.id;
+from employees as e 
+inner join departments as d on e.department_id=d.id
+inner JOIN locations as l on d.location_id = l.id;
+
+-- Wyświetl nazwę wszystkich działów oraz nazwę miast, w których znajdują się dane działy. Rekordy sortuj alfabetycznie po kolumnie miasto
+
+Select d.department_name, l.city
+from departments as d
+inner join locations as l on d.location_id=l.id
+Order by city;
+
+
+-- Wyświetl imię oraz datę urodzenia pracowników urodzony po 1 stycznia 1994 r. a także nazwę działu, w którym pracują.
+
+select concat(e.first_name, " ", e.last_name) as name_last_name, e.birth_date, d.department_name
+from employees as e
+inner join departments as d on e.department_id=d.id
+where birth_date>"1994-01-01";
+
+
